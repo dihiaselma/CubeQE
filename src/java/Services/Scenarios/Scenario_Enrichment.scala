0d9613babb2cabd9ea00372153a321063446773e
pc <er_saidoune@esi.dz>
2019-04-24 11:50:13 +0100

    mlk

--- src/java/Services/Scenarios/Scenario_Enrichment.scala
+++ src/java/Services/Scenarios/Scenario_Enrichment.scala
@@ -14,14 +14,14 @@ object Scenario_Enrichment extends App{
   /** 1. Unpersisting of annotated models **/
   var t_unpersisting: Long = System.currentTimeMillis()
   val modelsAnnotated: util.HashMap[String, Model] = TdbOperation.unpersistModelsMap(TdbOperation.dataSetAnnotated)
-  FileOperation.writeTimesInFile(timesFilePath, "Unpersisting for enrichment ", System.currentTimeMillis() - t_unpersisting)
-  FileOperation.writeQueriesNumberInFile(queriesNumberFilePath,"Enrichment: unpersisting ",modelsAnnotated.size())
+  FileOperation.writeInYAMLFile(timesFilePath, "Enrichment_Unpersisting", (System.currentTimeMillis() - t_unpersisting).toInt)
+  FileOperation.writeInYAMLFile(queriesNumberFilePath,"Enrichment_Unpersisting",modelsAnnotated.size())
 
 
   /** 2. Enrichment of annotated models **/
   var t_enrichment: Long = System.currentTimeMillis()
   enrichMDScehma(modelsAnnotated)
-  FileOperation.writeTimesInFile(timesFilePath, "Enrichment ", System.currentTimeMillis() - t_enrichment)
+  FileOperation.writeInYAMLFile(timesFilePath, "Enrichment", (System.currentTimeMillis() - t_enrichment).toInt)
 
 
 }
